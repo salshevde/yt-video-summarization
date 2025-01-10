68447355a3a5702c261237605eee7a169483e24f
Saloni Shevde <132834991+salshevde@users.noreply.github.com>
2025-01-10 12:42:03 +0530

    category

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,41 @@ import { NhostProvider } from '@nhost/react';
 import { nhost } from './lib/nhost';
 // require('dotenv').config();
 
+const cat_id =  {
+  "1": "Film & Animation",
+  "2": "Autos & Vehicles",
+  "10": "Music",
+  "15": "Pets & Animals",
+  "17": "Sports",
+  "18": "Short Movies",
+  "19": "Travel & Events",
+  "20": "Gaming",
+  "21": "Videoblogging",
+  "22": "People & Blogs",
+  "23": "Comedy",
+  "24": "Entertainment",
+  "25": "News & Politics",
+  "26": "Howto & Style",
+  "27": "Education",
+  "28": "Science & Technology",
+  "29": "Nonprofits & Activism",
+  "30": "Movies",
+  "31": "Anime/Animation",
+  "32": "Action/Adventure",
+  "33": "Classics",
+  "34": "Comedy",
+  "35": "Documentary",
+  "36": "Drama",
+  "37": "Family",
+  "38": "Foreign",
+  "39": "Horror",
+  "40": "Sci-Fi/Fantasy",
+  "41": "Thriller",
+  "42": "Shorts",
+  "43": "Shows",
+  "44": "Trailers"
+}
+
 function App() {
   const nhostClient = useNhostClient();
   const [session, setSession] = useState<NhostSession | null>(null)
@@ -414,7 +449,7 @@ function App() {
                             </h3>
                             <div className="flex gap-2 flex-wrap">
                               <span className="px-3 py-1 bg-gray-100 text-gray-600 rounded-full text-sm">
-                                {summary.category}
+                                {cat_id[summary.category]}
                               </span>
                               <span className="px-3 py-1 bg-gray-100 text-gray-600 rounded-full text-sm">
                                 {summary.language}
